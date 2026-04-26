ffbc4dfa8b8e6834c994f0993f7e49f1dfe3e21f
sofjushka <anonimks13@gmail.com>
2026-04-27 01:45:27 +0500

    refactor

--- public/js/announcements.js
+++ public/js/announcements.js
@@ -7,16 +7,18 @@ export async function renderAnnouncements() {
 
     let html = '<h3>Объявления</h3>';
 
-    html += `
-      <div class="panel-card">
-        <h4>Создать объявление</h4>
-        <form id="noticeCreateForm" class="simple-form">
-          <label>Заголовок:<input name="title" required></label>
-          <label>Текст:<textarea name="body" required></textarea></label>
-          <button type="submit" class="btn">Опубликовать объявление</button>
-        </form>
-      </div>
-    `;
+    if (window.currentUser?.role === 'admin') {
+        html += `
+        <div class="panel-card">
+            <h4>Создать объявление</h4>
+            <form id="noticeCreateForm" class="simple-form">
+            <label>Заголовок:<input name="title" required></label>
+            <label>Текст:<textarea name="body" required></textarea></label>
+            <button type="submit" class="btn">Опубликовать объявление</button>
+            </form>
+        </div>
+        `;
+    }
 
     html += `<ul>${announcements
         .map((a) => `<li><b>${a.title}</b> – ${a.body} <span class="muted">(${a.published_at})</span></li>`)
@@ -24,21 +26,23 @@ export async function renderAnnouncements() {
 
     panel.innerHTML = html;
 
-    const form = document.getElementById('noticeCreateForm');
-    form.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        const fd = new FormData(form);
-        try {
-            await createAnnouncement({
-                title: fd.get('title'),
-                body: fd.get('body'),
-            });
-            renderNotification('Объявление опубликовано', 'success');
-            form.reset();
-            await renderAnnouncements();
-        } catch (error) {
-            console.log(error);
-            renderNotification('Ошибка публикации объявления: ' + error.message);
-        }
-    });
+    if (window.currentUser?.role === 'admin') {
+        const form = document.getElementById('noticeCreateForm');
+        form.addEventListener('submit', async (event) => {
+            event.preventDefault();
+            const fd = new FormData(form);
+            try {
+                await createAnnouncement({
+                    title: fd.get('title'),
+                    body: fd.get('body'),
+                });
+                renderNotification('Объявление опубликовано', 'success');
+                form.reset();
+                await renderAnnouncements();
+            } catch (error) {
+                console.log(error);
+                renderNotification('Ошибка публикации объявления: ' + error.message);
+            }
+        });
+    }
 }
